1ce12838f282de69df353711aa397e914a76771b
SYCKGit <oviyangandhi@gmail.com>
2023-12-21 23:38:40 +0530

    Move templates to template/full

--- make.py
+++ make.py
@@ -4,7 +4,7 @@ parser.add_argument("name")
 parser.add_argument("-u", "--usaco", action="store_true")
 parser.add_argument("-f")
 args = parser.parse_args()
-with open(f"template/{'usaco' if args.usaco else 'normal'}.cpp") as f:
+with open(f"template/full/{'usaco' if args.usaco else 'normal'}.cpp") as f:
     template = f.read()
 with open(f"{args.name}/{args.name}.cpp", "w") as f:
     f.write(template.replace("{problem name}", args.name))
