ed22f18e5e43395e34ddd5d05b42c9dcc7f1cf6a
SYCKGit <oviyangandhi@gmail.com>
2024-05-22 12:35:10 +0530

    add bare template

--- make.py
+++ make.py
@@ -2,9 +2,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("name")
 parser.add_argument("-u", "--usaco", action="store_true")
+parser.add_argument("-b", "--bare", action="store_true")
 parser.add_argument("-f", action="store_true")
 args = parser.parse_args()
-with open(f"template/full/{'usaco' if args.usaco else 'normal'}.cpp") as f:
+
+with open(f"template/full/{(args.usaco and 'usaco') or (args.bare and 'bare') or 'normal'}.cpp") as f:
     template = f.read()
 with open(f"{args.name}/{args.name}.cpp", "w") as f:
     f.write(template.replace("{problem name}", args.name))
---
+++ template/full/bare.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
