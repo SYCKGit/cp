89e14d17e8557b0eb4f3e24da67cbfb61c1f5946
SYCKGit <oviyangandhi@gmail.com>
2025-06-10 16:13:02 +0530

    Update to new input file naming scheme

--- test/__main__.py
+++ test/__main__.py
@@ -1,3 +1,5 @@
+# TODO: Implement auto template generator
+# TODO: Implement random auto checker
 from .parser import Parser
 from .generator import Generator
 from argparse import ArgumentParser
@@ -10,14 +12,12 @@ def problem(pname: str):
         raise NotADirectoryError(pname)
 
 if __name__ == "__main__":
-    # TODO: Implement auto template generator
-    # TODO: Implement random auto checker
     parser = ArgumentParser()
     parser.add_argument("PROBLEM_NAME", type=problem)
     args = parser.parse_args()
     with open(f"{args.PROBLEM_NAME}/fmt.in") as f:
         try:
-            with open(f"{args.PROBLEM_NAME}/in.txt", "w") as p:
+            with open(f"{args.PROBLEM_NAME}/{args.PROBLEM_NAME}.in", "w") as p:
                 p.write(Generator(Parser(f.read())).generate())
         except Exception as e:
             print(f"SyntaxError: {e}")
